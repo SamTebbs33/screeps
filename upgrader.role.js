53386e052444dd6cadefba2d8b5caa9b74b8e4bd
Sam Tebbs <samuel.tebbs@gmail.com>
2023-11-15 01:02:18 +0000

    Make upgrader withdraw from extensions too

--- upgrader.role.js
+++ upgrader.role.js
@@ -1,10 +1,9 @@
 const upgraderRole = {
     run: function(creep) {
         if (!creep.memory.source) creep.memory.source = 0;
-        if (!creep.memory.mode) creep.memory.mode = "upgrading";
+        if (!creep.memory.mode) creep.memory.mode = "filling";
 
         const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-        const spawns = creep.room.find(FIND_MY_SPAWNS);
         
         if (creep.memory.mode == "upgrading" && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.mode = "filling";
@@ -14,21 +13,12 @@ const upgraderRole = {
         
         
         if (creep.memory.mode == "filling") {
-            var bestSpawn = 0;
-            var mostSpawn = 0;
-            for (var x in spawns) {
-                const energy = spawns[x].store[RESOURCE_ENERGY];
-                if (energy > mostSpawn) {
-                    bestSpawn = x;
-                    mostSpawn = energy;
-                }
-            }
-            creep.memory.source = bestSpawn;
+            const spawns = creep.room.find(FIND_MY_STRUCTURES, { filter: structure => structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION && structure.store[RESOURCE_ENERGY] > 0 });
             const src = spawns[creep.memory.source];
             if (creep.withdraw(src, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 if (creep.moveTo(src, { visualizePathStyle: { stroke: "#fff" } }) == ERR_NO_PATH) {
                     creep.memory.source++;
-                    if (creep.memory.source >= spawns.length)
+                    if (creep.memory.source >= Object.keys(spawns).length)
                         creep.memory.source = 0;
                 }
             }
