c38094ae84aa1506e02c038fbfc7980bb849c8c1
Sam Tebbs <samuel.tebbs@gmail.com>
2023-11-17 23:32:44 +0000

    Improve hauler target finding

--- src/hauler.role.js
+++ src/hauler.role.js
@@ -3,6 +3,7 @@ const b = require("behaviours");
 const haulerRole = {
     run: function(creep) {
         if (!creep.memory.source) creep.memory.source = "";
+        if (!creep.memory.dest) creep.memory.dest = "";
 
         var source = Game.getObjectById(creep.memory.source);
 
@@ -14,22 +15,41 @@ const haulerRole = {
         }
 
         if (creep.store[RESOURCE_ENERGY] < creep.store.getCapacity() && source) {
-            if (creep.pickup(source) == ERR_NOT_IN_RANGE)
-                creep.moveTo(source, { visualizePathStyle: { stroke: "#fff" } });
-        } else {
-            const targets = b.findEnergyStorage(creep.room);
-            var target = 0;
-            var targetSpace = 0;
-            for (var t in targets) {
-                var space = targets[t].store.getFreeCapacity(RESOURCE_ENERGY);
-                if (space > targetSpace) {
-                    target = t;
-                    targetSpace = space;
+            if (creep.pickup(source) == ERR_NOT_IN_RANGE) {
+                if (creep.moveTo(source, { visualizePathStyle: { stroke: "#fff" } }) == ERR_NO_PATH) {
+                    source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: function(res) {
+                        return res.resourceType == RESOURCE_ENERGY && res.id !== source.id;
+                    }});
+                    if (source) creep.memory.source = source.id;
                 }
             }
-
-            if (creep.transfer(targets[target], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                creep.moveTo(targets[target], { visualizePathStyle: { stroke: "#fff" } });
+            creep.memory.dest = "";
+        } else {
+            var target = Game.getObjectById(creep.memory.dest);
+            if (!target) {
+                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, { filter: function(res) {
+                    const type = res.structureType;
+                    return (type == STRUCTURE_SPAWN || type == STRUCTURE_EXTENSION) && res.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }});
+                if (target) creep.memory.dest = target.id;
+                else return;
+            }
+            const err = creep.transfer(target, RESOURCE_ENERGY);
+            if (err == ERR_NOT_IN_RANGE) {
+                if (creep.moveTo(target, { visualizePathStyle: { stroke: "#fff" } }) == ERR_NO_PATH) {
+                    target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, { filter: function(res) {
+                        const type = res.structureType;
+                        return (type == STRUCTURE_SPAWN || type == STRUCTURE_EXTENSION) && res.id !== target.id && res.store.getFreeCapacity(RESOURCE_ENERGY) > 0; 
+                    }});
+                    if (target) creep.memory.dest = target.id;
+                }
+            } else if (err == ERR_FULL) {
+                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, { filter: function(res) {
+                    const type = res.structureType;
+                    return (type == STRUCTURE_SPAWN || type == STRUCTURE_EXTENSION) && res.id !== target.id && res.store.getFreeCapacity(RESOURCE_ENERGY) > 0; 
+                }});
+                if (target) creep.memory.dest = target.id;
+            }
         }
     }
 }
